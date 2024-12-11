048a7f49b851c6110d70021bea9e8f0b01c06f4b
Mayank <mayankmchandratre@gmail.com>
2024-12-11 22:45:22 +0530

    add

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -14,6 +14,7 @@ import ViewTeachers from "./Section/School/view-teachers"
 const isAuthorized = () => {
   const token = localStorage.getItem("token")
   if (token) {
+    
     return true
   }
   return false
